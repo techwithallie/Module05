da8fc7b7e30193207c6144cf3356f3ba8793ccf2
techwithallie <127265963+techwithallie@users.noreply.github.com>
2024-01-29 20:54:56 -0800

    cleaned up code&changed if else loop to a for loop

--- js/example.js
+++ js/example.js
@@ -5,20 +5,16 @@ var msg = '';              // Message
 
 table = parseInt(table); //converting user input to integer
 
-if (operator === 'addition')
+ if (operator === 'multiplication'); //changed operator to multiplication
  {
-  // Do addition
-  while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} else {
-  // Do multiplication
-  while (i < 11) {
+  // changed to a for loop so there was no "else" extra code
+  for (; i < 11 ;) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
 }
+
+
 
 // Write the message into the page
 var el = document.getElementById('blackboard');
